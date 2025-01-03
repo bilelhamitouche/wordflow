a2bd6aad773ca013a6e3ba5e1403e2bb55506f96
Bilel Hamitouche <bilelhamitouche>
2025-01-03 13:18:29 +0100

    added authentication controller(register, validation, login)

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,7 @@
 import { body, validationResult } from "express-validator";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "../prisma/prismaClient.js";
 
 const alphanumericError = "should only contain letters and digits";
 const emailError = "should be a valid email address";
@@ -25,7 +27,7 @@ const register = [
       return res.status(400).json({ errors: errors.array() });
     }
     const { username, email, password } = req.body;
-    bcrypt.hash(password, 10, (err, hashedPassword) => {
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
       if (err) {
         return next(err);
       } else {
@@ -34,10 +36,10 @@ const register = [
             data: {
               username,
               email,
-              password: hashedPassword
-            }
-          })
-          res.status(201).json({ message: "Account created successfully" })
+              password: hashedPassword,
+            },
+          });
+          res.status(201).json({ message: "Account created successfully" });
         } catch (err) {
           return next(err);
         }
@@ -47,7 +49,29 @@ const register = [
 ];
 
 async function login(req, res) {
-
+  const { username, email, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(404).json({ error: "Invalid username or email" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid password" });
+    }
+    const payload = { id: user.id, email: user.email, role: user.role };
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+    res.json({ token });
+  } catch (err) {
+    return res.status(500).json({ error: "Internal server error" });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-export { register };
+export { register, login };
