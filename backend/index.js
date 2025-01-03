b9e52d444bdef74f82983f2eca6aa6a579df2c53
Bilel Hamitouche <bilelhamitouche>
2025-01-03 13:18:50 +0100

    mounted the auth routes into the main app

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
 import cors from "cors";
+import authRouter from "./routes/authRouter.js";
+import "./config/passport.js";
 config();
 
 const app = express();
@@ -9,6 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
