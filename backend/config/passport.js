3fc05bc0aea83ea141d3df7a56298cec96e1774f
Bilel Hamitouche <bilelhamitouche>
2025-01-03 13:17:44 +0100

    update passportjs config

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,6 +1,7 @@
 import passport from "passport";
 import { ExtractJwt, Strategy as JwtStrategy } from "passport-jwt";
 import { config } from "dotenv";
+import prisma from "../prisma/prismaClient.js";
 config();
 
 const opts = {};
@@ -10,6 +11,16 @@ opts.secretOrKey = process.env.JWT_SECRET;
 passport.use(
   new JwtStrategy(opts, async (jwt_payload, done) => {
     try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: jwt_payload.email,
+        },
+      });
+      if (!user) {
+        return done(null, false);
+      } else {
+        return done(null, user);
+      }
     } catch (err) {
       return done(err, false);
     } finally {
