bb47a8f6517a6a8772a6d219953f88ea92f868b0
Bilel Hamitouche <bilelhamitouche>
2025-01-04 01:12:05 +0100

    mounted the posts router in the main app

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import authRouter from "./routes/authRouter.js";
+import postsRouter from "./routes/postsRouter.js";
 import "./config/passport.js";
 config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRouter);
+app.use("/api/posts", postsRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
