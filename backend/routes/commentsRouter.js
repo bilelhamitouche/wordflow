9f82b34e635345721ee83e9ef1ed9b449a5b3eea
Bilel Hamitouche <bilelhamitouche>
2025-01-05 13:41:37 +0100

    added route for updating a component

--- backend/routes/commentsRouter.js
+++ backend/routes/commentsRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {
   createComment,
+  deleteComment,
   getComments,
+  updateComment,
 } from "../controllers/commentController.js";
 import { authenticateJWT } from "../middleware/authMiddleware.js";
 
@@ -9,5 +11,7 @@ const commentsRouter = Router({ mergeParams: true });
 
 commentsRouter.get("/", authenticateJWT, getComments);
 commentsRouter.post("/", authenticateJWT, createComment);
+commentsRouter.patch("/:commentId", authenticateJWT, updateComment);
+commentsRouter.delete("/:commentId", authenticateJWT, deleteComment);
 
 export default commentsRouter;
